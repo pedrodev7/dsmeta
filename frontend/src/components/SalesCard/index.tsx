d2b8a1bc9440d4f0c9c572fba697c6ab0124edcc
Pedro Henrique <pedrohenrique096k@gmail.com>
2022-09-09 17:07:23 -0300

    Primeira requisição com useEffect. Utilizando Axios

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -7,8 +8,18 @@ import './styles.css';
 
 function SalesCard() {
     
-    const [minDate, setMinDate] = useState(new Date());
-    const [maxDate, setMaxDate] = useState(new Date());
+    const min = new Date(new Date().setDate(new Date().getDate()- 365));
+    const max = new Date();
+
+    const [minDate, setMinDate] = useState(new Date(min));
+    const [maxDate, setMaxDate] = useState(new Date(max));
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/sales")
+        .then(response => {
+            console.log(response.data);
+        })
+    }, [])
     
     return (
         <div className="dsmeta-card">
