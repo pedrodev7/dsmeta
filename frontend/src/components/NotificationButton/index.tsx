60801ac75d7b3c0bad8a8c55d0e090e948752646
Pedro Henrique <pedrohenrique096k@gmail.com>
2022-09-09 21:38:51 -0300

    Enviando notificação pelo button

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,11 +1,24 @@
 import icon from '../../assets/notification-icon.svg'
+import axios from 'axios'
 
 import './styles.css'
+import { BASE_URL } from '../../utils/request'
 
-function NotificationButton() {
+type Props = {
+    saleId: number
+}
+
+function handleClick(id: number) {
+    axios.get(`${BASE_URL}/sales/${id}/notification`)
+    .then(response => {
+        
+    })
+}
+
+function NotificationButton( {saleId} : Props ) {
 
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
             <img src={icon} alt="Notificar" />
         </div>
     )
